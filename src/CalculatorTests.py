7339057f076ea23e676631d86b583d2327f74e0c
Brittani Rubil <bmb23@njit.edu>
2019-11-03 21:34:39 -0500

    Feature: Setup Calculator with add method

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,6 +1,7 @@
 import unittest
 from Calculator import Calculator
 
+
 class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         calculator = Calculator()
@@ -9,6 +10,11 @@ class MyTestCase(unittest.TestCase):
     def test_results_property_calculator(self):
         calculator = Calculator()
         self.assertAlmostEqual(calculator.result, 4)
+
+    def test_add_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2, 2), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
