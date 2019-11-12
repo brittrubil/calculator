3028fda64fc11668215368853bcb70aa2aff6993
Brittani Rubil <bmb23@njit.edu>
2019-11-12 16:16:28 -0500

    Task: Added the pprint

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,6 +1,7 @@
 import unittest
 from Calculator import Calculator
 from CsvReader import CsvReader
+from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
@@ -10,17 +11,16 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_property(self):
-        self.assertEqual(self.calculator.result, 0)
-
     def test_subtraction(self):
         test_data = CsvReader('/src/subtraction.csv').data
+        pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_addition(self):
         test_data = CsvReader('/src/addition.csv').data
+        pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
@@ -33,21 +33,27 @@ class MyTestCase(unittest.TestCase):
 
     def test_division(self):
         test_data = CsvReader('/src/division.csv').data
+        pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_square(self):
         test_data = CsvReader('/src/square.csv').data
+        pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_square_root(self):
         test_data = CsvReader('/src/squareroot.csv').data
+        pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.sqrt(row['Value 1']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
