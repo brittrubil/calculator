d1efd992fe37cb85f909173dc1192b3721258bd4
Brittani Rubil <bmb23@njit.edu>
2019-11-12 15:47:55 -0500

    Fix: The Calculator for Division

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -33,9 +33,9 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_division(self):
-        test_data = CsvReader('/src/multiplication.csv').data
+        test_data = CsvReader('/src/division.csv').data
         for row in test_data:
-            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
 
