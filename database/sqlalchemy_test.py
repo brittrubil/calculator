684c12de5078ebe11ec1afe42abef175f744bccc
Brittani Rubil <bmb23@njit.edu>
2019-12-12 14:49:42 -0500

    Feature: Finish Creating Session

--- database/sqlalchemy_test.py
+++ database/sqlalchemy_test.py
@@ -1,3 +1,8 @@
 from sqlalchemy.orm import create_engine, Session
 engine = create_engine("postgres+psycopg2://postgres:pass@localhost/mydb")
+from sqlalchemy.orm import sessionmaker, Session
+
+Session = sessionmaker(bind=engine)
 session = Session(bind=engine)
+
+session = Session ()
