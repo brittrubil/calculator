5266ec2d17a9e0d4851326205ae4ae91dc5497e0
Brittani Rubil <bmb23@njit.edu>
2019-12-12 15:44:07 -0500

    Task: Using offset method

--- database/sqlalchemy_test.py
+++ database/sqlalchemy_test.py
@@ -172,3 +172,5 @@ session.query(Item).filter(not_(Item.name.like("W%"))).all()
 
 session.query(Customer).limit(2).all()
 session.query(Customer).filter(Customer.address.ilike("%avenue")).limit(2).all()
+
+print(session.query(Customer).limit(2).offset(2))
